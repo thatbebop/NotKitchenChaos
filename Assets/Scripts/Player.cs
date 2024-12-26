7138dde3f9b03d3792ed9bd3145d50b9e1460b13
thatbebop <andcas222@gmail.com>
2024-12-26 02:37:14 +0100

    StoveCounter, PlatesCounter and ContainerCounter added. The player can pickup plates, cook meat on the Stove (with the posibility of burning the meat also) and the ProgressBarUI element can be used for multiple Counters. SO added.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,6 +39,15 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     private void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
+        gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
+    }
+
+    private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.InteractAlternate(this);
+        }
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
@@ -106,7 +115,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             // cannot move towards moveDir
             // attempt only x movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
             {
@@ -118,7 +127,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
                 // attempt only z movement
 
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
